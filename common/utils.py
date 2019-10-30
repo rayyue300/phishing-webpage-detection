7752da5bc01cfd1667fb1067b87b5856c69051a7
Rayyue300 <rayyue300@9pc.us>
2019-10-30 21:33:15 +0800

    Added docstrings for isAscii and getDomainFromUrl

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlparse
 
 def isAscii(s: str) -> bool:
+    """Return True if s contains ASCII characters only."""
     try:
         s.encode('ascii')
         return True
@@ -8,4 +9,5 @@ def isAscii(s: str) -> bool:
         return False
 
 def getDomainFromUrl(u: str) -> str:
+    """Return the domain part of the given URL"""
     return urlparse(u).netloc
