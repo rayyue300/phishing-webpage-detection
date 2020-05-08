c29306c3fb8fefc04e24181da8c46ed2b3101b66
Rayyue300 <rayyue300@9pc.us>
2020-05-08 20:17:29 +0800

    Added exception

--- features_extraction/keyword.py
+++ features_extraction/keyword.py
@@ -5,43 +5,61 @@ from common import utils
 # General Function
 def isStringInPage(url: str, searchTarget: str) -> int:
     """Return 1 if the the searchTarget exists in the page of the given URL"""
-    source = utils.getLoadedHtmlFromUrl(url)
+    try:
+        source = utils.getLoadedHtmlFromUrl(url)
+    except:
+        return -1
     return 1 if searchTarget in source else 0
 
 def isPageContainingLogin(url: str) -> int:
     """Return 1 if the page contains 'login' in the page of the given URL"""
-    source = utils.getLoadedHtmlFromUrl(url)
-    result1 = 'login' in source
-    result2 = 'Login' in source
+    try:
+        source = utils.getLoadedHtmlFromUrl(url)
+        result1 = 'login' in source
+        result2 = 'Login' in source
+    except:
+        return -1
     return 1 if (result1 or result2) else 0
 
 def isPageContainingCreditCard(url: str) -> int:
     """Return 1 if the page contains 'credit card' in the page of the given URL"""
-    source = utils.getLoadedHtmlFromUrl(url)
-    result1 = 'credit card' in source
-    result2 = 'credit-card' in source
-    result3 = 'Credit Card' in source
+    try:
+        source = utils.getLoadedHtmlFromUrl(url)
+        result1 = 'credit card' in source
+        result2 = 'credit-card' in source
+        result3 = 'Credit Card' in source
+    except:
+        return -1
     return 1 if (result1 or result2 or result3) else 0
 
 def isPageContainingDownload(url: str) -> int:
     """Return 1 if the page contains 'download' in the page of the given URL"""
-    source = utils.getLoadedHtmlFromUrl(url)
-    result1 = 'download' in source
-    result2 = 'Download' in source
+    try:
+        source = utils.getLoadedHtmlFromUrl(url)
+        result1 = 'download' in source
+        result2 = 'Download' in source
+    except:
+        return -1
     return 1 if (result1 or result2) else 0
 
 def isPageContainingUrgent(url: str) -> int:
     """Return 1 if the page contains 'urgent' in the page of the given URL"""
-    source = utils.getLoadedHtmlFromUrl(url)
-    result1 = 'urgent' in source
-    result2 = 'Urgent' in source
-    result3 = 'URGENT' in source
+    try:
+        source = utils.getLoadedHtmlFromUrl(url)
+        result1 = 'urgent' in source
+        result2 = 'Urgent' in source
+        result3 = 'URGENT' in source
+    except:
+        return -1
     return 1 if (result1 or result2 or result3) else 0
 
 def isPageContainingActionRequired(url: str) -> int:
     """Return 1 if the page contains 'action required' in the page of the given URL"""
-    source = utils.getLoadedHtmlFromUrl(url)
-    result1 = 'action required' in source
-    result2 = 'Action Required' in source
-    result3 = 'ACTION REQUIRED' in source
+    try:
+        source = utils.getLoadedHtmlFromUrl(url)
+        result1 = 'action required' in source
+        result2 = 'Action Required' in source
+        result3 = 'ACTION REQUIRED' in source
+    except:
+        return -1
     return 1 if (result1 or result2 or result3) else 0
